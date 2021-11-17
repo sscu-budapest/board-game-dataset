af59d0b6b3531b8ab5f732b7407d0ff62594e700
kbenya <kbb1795@gmail.com>
2021-11-17 16:50:59 +0100

    add metadata

--- src/namespace_metadata.py
+++ src/namespace_metadata.py
@@ -1,8 +1,105 @@
-from sscutils import ScruTable, TableFeaturesBase
+import datetime as dt
 
+from sscutils import (
+    BaseEntity,
+    CompositeTypeBase,
+    IndexBase,
+    ScruTable,
+    TableFeaturesBase,
+)
 
-class RecordFeatures(TableFeaturesBase):
+class BoardGame(BaseEntity):
+    """Board Game from BGG"""
     pass
 
+class Person(BaseEntity):
+    pass
 
-records_table = ScruTable(RecordFeatures)
+class User(Person):
+    """BGG User"""
+    pass
+
+class BoardGameIndex(IndexBase):
+    bg_id = int
+
+class UserIndex(IndexBase):
+    user_id = str
+
+class SubgroupIndex(IndexBase):
+    subgroup_id = str
+
+class MicrobadgeIndex(IndexBase):
+    micro_id = int
+
+class RatingIndex(IndexBase):
+    bg_id = BoardGameIndex
+    user_name = UserIndex
+
+class PlayerNumberType(CompositeTypeBase):
+    minplayers = int
+    maxplayers = int
+
+class RatingInfoType(CompositeTypeBase):
+    num_ratings = int
+    avg_rating = float
+    bgg_rating = float
+    stddev_rating = float
+
+class WeightType(CompositeTypeBase):
+    num_weights = int
+    average_weight = float
+
+class OwnershipType(CompositeTypeBase):
+    owned = int
+    trading = int
+    wanting = int
+    wishing = int
+
+class BoardGameFeatures(TableFeaturesBase):
+    bgg_rank = int
+    name = str
+    url = str
+    release_year = int
+    description = str
+    player_num = PlayerNumberType
+    rating_info = RatingInfoType
+    weight = WeightType
+    ownership = OwnershipType
+
+class UserFeatures(TableFeaturesBase):
+    user_url = str
+    country = str
+
+class RatingFeatures(TableFeaturesBase):
+    rating = float
+    date = dt.datetime
+    comment_text = str
+    summary_item_meta = str
+
+class SubgroupFeatures(TableFeaturesBase):
+    subgroup_type = str
+    subgroup_name = str
+    description = str
+
+class MicrobadgeFeatures(TableFeaturesBase):
+    micro_name = str
+    micro_url = str
+    description = str
+
+class UserMicroFeatures(TableFeaturesBase):
+    user_id = UserIndex
+    microbadge = MicrobadgeIndex
+
+class BGSubgroupFeatures(TableFeaturesBase):
+    bg_id = BoardGameIndex
+    subgroup_id = SubgroupIndex
+    value = str
+
+boardgame_table = ScruTable(BoardGameFeatures, BoardGameIndex, subject_of_records= BoardGame)
+user_table = ScruTable(UserFeatures, UserIndex, subject_of_records= User)
+rating_table = ScruTable(RatingFeatures, RatingIndex)
+subgroup_table = ScruTable(SubgroupFeatures, SubgroupIndex)
+microbadge_table = ScruTable(MicrobadgeFeatures, MicrobadgeIndex)
+user_micro_table = ScruTable(UserMicroFeatures)
+bg_subgroup_table = ScruTable(BGSubgroupFeatures)
+
--- src/update_data.py
+++ src/update_data.py
@@ -1,2 +1,29 @@
-def update_data():
-    pass
+import pandas as pd
+from colassigner import get_all_cols
+from sscutils import dump_dfs_to_tables
+
+import src.namespace_metadata as ns
+
+
+def update_data(data_root = "C:/programozas/bgg/data/final_table"):
+
+    boardgame_df = pd.read_json(f"{data_root}/boardgame.json")
+    rating_df = pd.read_json(f"{data_root}/rating.json")
+    user_df = pd.read_json(f"{data_root}/user.json")
+    bg_subgroup_df = pd.read_json(f"{data_root}/bg_subgroup.json")
+    microbadge_df = pd.read_json(f"{data_root}/microbadge.json")
+    subgroup_df = pd.read_json(f"{data_root}/subgroup.json")
+    user_micro_df = pd.read_json(f"{data_root}/user_micro.json")
+
+    dump_dfs_to_tables(
+        None,
+        [
+            (boardgame_df, ns.boardgame_table),
+            (rating_df, ns.rating_table),
+            (user_df, ns.user_table),
+            (bg_subgroup_df, ns.bg_subgroup_table),
+            (microbadge_df, ns.microbadge_table),
+            (subgroup_df, ns.subgroup_table),
+            (user_micro_df, ns.user_micro_table)
+        ],
+    )
